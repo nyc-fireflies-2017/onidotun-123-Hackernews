45f7285bc9ee9e0f1aef9a67c20b2774183db9a9
Adedotun Oni <email": null>
2017-03-06 12:02:49 -0500

    Deletion works

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,6 +23,12 @@ class CategoriesController < ApplicationController
       redirect_to categories_path
     end
 
+    def destroy
+      @category = Category.find(params[:id])
+      @category.destroy
+      redirect_to categories_path
+    end
+
       private
     def category_params
       params.require(:category).permit(:name)
