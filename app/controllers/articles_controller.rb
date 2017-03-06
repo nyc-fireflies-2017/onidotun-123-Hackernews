42b447dae51da7300422e0576f0480bcf1ec24ab
Adedotun Oni <email": null>
2017-03-06 11:16:08 -0500

    Create new category page with a form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,7 @@
 class ArticlesController < ApplicationController
+
+  # def index
+  #   @categories = Category.all
+  #   @articles = @categories.articles.all
+  # end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,8 @@ class CategoriesController < ApplicationController
     def index
     @categories = Category.all
     end
+
+    def new
+      @category = Category.new
+    end
 end
