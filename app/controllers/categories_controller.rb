0e18041cd720a41c97ea6b8d0c4c5e6b9823edab
Adedotun Oni <email": null>
2017-03-06 11:47:36 -0500

    Create Update page and it works and updates page accordingly.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,12 +12,20 @@ class CategoriesController < ApplicationController
       Category.create(category_params)
       redirect_to categories_path
     end
+
+    def edit
+      @category = Category.find(params[:id])
+    end
+
+    def update
+      @category = Category.find(params[:id])
+      @category.update_attributes(category_params)
+      redirect_to categories_path
+    end
+
       private
     def category_params
       params.require(:category).permit(:name)
     end
 
-    def edit
-
-    end
 end
