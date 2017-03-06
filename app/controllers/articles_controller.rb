f62bdbe2d250fc8bcaa9b1be06931268aa8a667d
Adedotun Oni <email": null>
2017-03-06 14:23:58 -0500

    Create a new article link and controller routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,29 @@ class ArticlesController < ApplicationController
   def index
     @category = Category.find(params[:category_id])
     @articles = @category.articles.all
+  end
+
+  def new
+    @category = Category.find(params[:category_id])
+    @article = @category.articles.new
 
   end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @article = @category.articles.new(article_params)
+
+      if @article.save
+        redirect_to category_articles_path(@category.id)
+      else
+        render "new"
+      end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
 
 end
